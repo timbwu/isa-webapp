2af878d7e8d7215f3800038299d11393f9ef7e74
timbwu <timwu767@gmail.com>
2021-03-29 15:29:53 -0700

    dummy endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 3 POST
+// 2 DELETE
+// 2 PUT
+// 1 GET
+
 app.get("/color", (req, res) => {
     res.sendFile(path.join(__dirname + "/../frontend/index.html"))
 })
@@ -14,10 +19,13 @@ app.get("/color", (req, res) => {
 app.get("/shape", (req, res) => {
     res.sendFile(path.join(__dirname + "/../frontend/index.html"))
 })
+// threejs shapes? create/store/update
 
 app.get("/gallery", (req, res) => {
     res.sendFile(path.join(__dirname + "/../frontend/index.html"))
 })
+// submit photos/thumbnail/3dshapes?
+// display/get/edit shape
 
 app.get("/home", (req, res) => {
     console.log(__dirname)
