5d5c2c52c80365a811f84130c2e455c316c16ae6
le-rich <richardle1997@gmail.com>
2021-03-30 01:24:33 -0700

    3D first pass on globe

--- frontend/script.js
+++ frontend/script.js
@@ -38,43 +38,67 @@ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
+// const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// directionalLight.shadow.camera.near = 2
+// directionalLight.shadow.camera.far = 4
+// scene.add(directionalLightCameraHelper)
+
 // Texture Loading
 const loadingManager = new THREE.LoadingManager()
-loadingManager.onstart = () => {
-
-}
-
-loadingManager.onLoaded = () => {
-
-}
 const textureLoader = new THREE.TextureLoader(loadingManager)
+loadingManager.onstart = () => {}
+loadingManager.onLoaded = () => {}
+
 const earthColorTexture = textureLoader.load('/frontend/textures/earth/Albedo.jpg')
 const earthBumpTexture = textureLoader.load('/frontend/textures/earth/Bump.jpg')
 const earthCloudTexture = textureLoader.load('/frontend/textures/earth/Clouds.png')
+const earthLightsTexture = textureLoader.load('/frontend/textures/earth/Night_Lights.png')
+
+const circleParticle = textureLoader.load('/frontend/textures/particles/circle.png')
+const flareParticle = textureLoader.load('/frontend/textures/particles/flare.png')
 
 
+// Earth and Cloud Geo
 const earthGeo = new THREE.SphereGeometry(1, 32, 32)
-const earthMaterial = new THREE.MeshStandardMaterial( {map: earthColorTexture } );
-const sphere = new THREE.Mesh(earthGeo, earthMaterial)
-scene.add(sphere)
+const earthMaterial = new THREE.MeshStandardMaterial( {map: earthColorTexture, bumpMap: earthBumpTexture, bumpScale: 0.05} );
+const earthSphere = new THREE.Mesh(earthGeo, earthMaterial)
+scene.add(earthSphere)
 
-const cloudGeo = new THREE.SphereGeometry(1.01, 32, 32)
+const wireGeo = new THREE.SphereGeometry(1.015, 32, 32)
+const wireMaterial = new THREE.MeshStandardMaterial( {wireframe: true, opacity: 0.15, emissiveIntensity: 5, transparent: true} );
+const wireSphere = new THREE.Mesh(wireGeo, wireMaterial)
+scene.add(wireSphere)
+
+
+const cloudGeo = new THREE.SphereGeometry(1.025, 32, 32)
 const cloudMaterial = new THREE.MeshStandardMaterial( {alphaMap: earthCloudTexture, map: earthCloudTexture, transparent: true})
 const cloudSphere = new THREE.Mesh(cloudGeo, cloudMaterial)
 scene.add(cloudSphere)
 
+// Star Particles
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 3000
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(15, 15),
-    new THREE.MeshBasicMaterial({
-    })
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 2.5
-scene.add(plane)
+for (let i = 0; i < count * 3; i++){
+    positions[i] = (Math.random() - 0.5) * 10
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.15
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.map = flareParticle
+particlesMaterial.sizeAttenuation = true
+particlesMaterial.transprent = true
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 
 // Camera
-const camera = new THREE.PerspectiveCamera(40, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = -3
 scene.add(camera)
 
@@ -82,14 +106,22 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.enablePan = false
-controls.enableZoom = false
+controls.enableZoom = false;
 
 // Renderer
  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(0x0, 0);
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+directionalLight.castShadow = true
+earthSphere.receiveShadow = true
+cloudSphere.castShadow = true
 
 // Update Loop
  const clock = new THREE.Clock()
@@ -98,8 +130,13 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
      const elapsedTime = clock.getElapsedTime()
 
     // Update controls
+    earthSphere.rotation.y = elapsedTime * -0.008
+    wireSphere.rotation.y = elapsedTime * 0.01
+    wireSphere.rotation.x = elapsedTime * 0.01
+    cloudSphere.rotation.y = elapsedTime * 0.01
+    
+    // Update controls
     controls.update()
-    cloudSphere.rotation.y += clock.getDelta() * 1.2;
 
     // Render
     renderer.render(scene, camera)
