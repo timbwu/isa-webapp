50472ed91cdb850af0e1112486bd2a809a752b68
le-rich <richardle1997@gmail.com>
2021-03-29 20:26:49 -0700

    Added threeJS boilerplate for basic scene

--- backend/server.js
+++ backend/server.js
@@ -6,35 +6,36 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/frontend/', express.static(path.join(__dirname, '../frontend')))
+app.use('/build/', express.static(path.join(__dirname, '../node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, '../node_modules/three/examples/jsm')))
+app.use('/dat.gui/', express.static(path.join(__dirname, '../node_modules/dat.gui')))
 // 3 POST
 // 2 DELETE
 // 2 PUT
 // 1 GET
 
 app.get("/color", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"))
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 
 app.get("/shape", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"))
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 // threejs shapes? create/store/update
 
 app.get("/gallery", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"))
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 // submit photos/thumbnail/3dshapes?
 // display/get/edit shape
 
 app.get("/home", (req, res) => {
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"))
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 
 app.get("/", (req, res) => {
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"))
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 
 app.listen(3030, () => {
---
+++ frontend/script.js
@@ -0,0 +1,65 @@
+import * as THREE from '/build/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls.js'
+import * as dat from '/dat.gui/build/dat.gui.module.js'
+
+
+// THREE Setup
+const gui = new dat.GUI()
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+// Screen Resizing 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const geometry = new THREE.SphereGeometry(1, 32, 32)
+const material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = -3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+ const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Update Loop
+ const clock = new THREE.Clock()
+ const tick = () =>
+ {
+     const elapsedTime = clock.getElapsedTime()
+
+     // Update controls
+     controls.update()
+ 
+     // Render
+     renderer.render(scene, camera)
+ 
+     // Call tick again on the next frame
+     window.requestAnimationFrame(tick)
+ }
+ 
+ tick()
