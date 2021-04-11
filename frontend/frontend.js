b15ccb10b060fa16349f3e622a7813d5961f6d17
le-rich <richardle1997@gmail.com>
2021-04-10 18:02:35 -0700

    Reset the path for frontend to relative dir

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/frontend/', express.static(path.join('./')))
 app.use('/build/', express.static(path.join(__dirname, '../node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, '../node_modules/three/examples/jsm')))
 app.use('/dat.gui/', express.static(path.join(__dirname, '../node_modules/dat.gui')))
